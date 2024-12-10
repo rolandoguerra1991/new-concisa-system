f891010a75c6a1045e374ae7f5af4bc258dce59e
rolando guerra <rolandoguerra1991@gmail.com>
2024-12-10 14:14:31 -0400

    added users module

---
+++ app/Filament/Resources/UserResource/Pages/ManageUsers.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Filament\Resources\UserResource\Pages;
+
+use App\Filament\Resources\UserResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ManageRecords;
+use Filament\Support\Enums\MaxWidth;
+
+class ManageUsers extends ManageRecords
+{
+    protected static string $resource = UserResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make()
+                ->modalWidth(MaxWidth::Medium),
+        ];
+    }
+}
