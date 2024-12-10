54d6e1b758749d093812929c13dc1a65c359b008
rolando guerra <rolandoguerra1991@gmail.com>
2024-12-10 11:51:34 -0400

    added categories module

---
+++ app/Models/Category.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    protected $fillable = ['name'];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'name' => 'string',
+        ];
+    }
+}
